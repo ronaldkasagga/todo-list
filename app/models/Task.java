718fe1e449e8f536d41e740aa1cb874c793114b0
Ronald Kasagga <ronald.kasagga.k3@gmail.com>
2014-03-29 16:43:51 +0300

    Ronald Kasagga/James Katarikawe
    additions
    -------------------------
    - added list all Taska method
    - added a method to find tasks by id in Task
    
    changes
    -------------------------
    - made Task an entity
    - modified application.conf to permit models and to use the h2 memory database

--- app/models/Task.java
+++ app/models/Task.java
@@ -1,19 +1,24 @@
 package models;
 
+import play.db.ebean.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-/**
- * Created by sagas on 3/22/14.
- */
-public class Task {
-    private int id;
+
+@Entity
+public class Task extends Model {
+    @Id
+    private long id;
     private String name;
     private String description;
+    public static Model.Finder<Long, Task> find = new Model.Finder(Long.class, Task.class);
     
     public Task(String name, String description){
         this.name = name;
         this.description = description;
-
     }
 
 
@@ -24,4 +29,15 @@ public class Task {
     public String getDescription() {
         return description;
     }
+
+    public static List<Task> all() {
+        return find.all();
+    }
+
+    public static Task findbyId(long l) {
+        return find.byId(l);
+    }
+
+
+
 }
--- test/TaskTest.java
+++ test/TaskTest.java
@@ -1,10 +1,14 @@
 import models.Task;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.running;
 
 /**
  * Created by sagas on 3/22/14.
@@ -29,6 +33,29 @@ public class TaskTest {
         Task sagasTask = new Task("Go to Church", description);
         //then
         assertThat(description, is(sagasTask.getDescription()));
+    }
+
+    public void makeDummyTasks(){
+        Task firstTask = new Task("Go to Church","");
+        firstTask.save();
+        Task secondTask = new Task("Buy Supper","");
+        secondTask.save();
+    }
+
+    @Test
+    public void canTaskBeSavedInDatabase(){
+        running(fakeApplication(),
+            new Runnable(){
+                public void run(){
+                    makeDummyTasks();
+                    List<Task> allTasks= Task.all();
+                    assertThat(allTasks.size(), is(2));
+                    Task task = Task.findbyId(1);
+                    assertThat(task.getName(), is("Go to Church"));
+                }
+            }
+        );
+
     }
 
 
